f084310b35b354920ca04e94eea5b0ca91118492
corodriguez <crorodriguezro@gmail.com>
2026-07-27 19:57:48 -0500

    Revert "xdisp-p0.2: skip dropped offscreen frame completion"
    This reverts commit 8b19bc768b51699dca245c1e4436715f30e5298b.

--- src/platforms/android/server/gl_context.cpp
+++ src/platforms/android/server/gl_context.cpp
@@ -19,7 +19,6 @@
 #include "gl_context.h"
 #include "buffer.h"
 #include "framebuffer_bundle.h"
-#include "gud_render_only_control.h"
 #include "android_format_conversion-inl.h"
 #include "mir/graphics/display_report.h"
 #include "mir/graphics/gl_config.h"
@@ -267,8 +266,7 @@ void mga::FramebufferGLContext::swap_buffers() const
 {
     if (offscreen)
     {
-        if (mga::should_finish_gud_offscreen_frame())
-            glFinish();
+        glFinish();
     }
     else if (eglSwapBuffers(egl_display, egl_surface) == EGL_FALSE)
     {
